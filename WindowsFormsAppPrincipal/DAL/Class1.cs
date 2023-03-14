8025e7784d8138648091cd2ac18b07a2fe0da87f
André Victor <andrevictor03092001@gmail.com>
2023-03-14 16:46:34 -0300

    Criação da Models, DALL, BLL
    Criação da DALL, Models e BLL

---
+++ WindowsFormsAppPrincipal/DAL/Class1.cs
@@ -0,0 +1,34 @@
+﻿using Models;
+using System.Collections.Generic;
+using System;
+
+namespace DAL
+{
+    public class GrupoUsuarioDALL
+    {
+        public void Inserir(GrupoUsuario _grupoUsuario)
+        {
+
+        }
+        public List<GrupoUsuario> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+        public List<GrupoUsuario> BuscarPorNomeUsuario(string _nomeUsuario)
+        {
+            throw new NotImplementedException();
+        }
+        public List<GrupoUsuario> BuscarPorId(int _id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Alterar(GrupoUsuario _grupoUsuario)
+        {
+
+        }
+        public void Excluir(GrupoUsuario _id)
+        {
+
+        }
+    }
+}
